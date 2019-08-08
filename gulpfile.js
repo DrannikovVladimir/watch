2ab38b4706bf600f602a2f9c68df7b833c3fde13
Vladimir Drannikov <krowen@inbox.ru>
2019-08-08 11:59:44 +0600

    Добавил шрифты, сделал разметку шапки и стилизовал

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use sctrict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('sass', function () {
+  return gulp.src('source/sass/style.scss')
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('source/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('server', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'source'
+    }
+  });
+
+  gulp.watch('source/sass/**/*.scss', gulp.series('sass'));
+  gulp.watch('source/**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('start', gulp.series('sass', 'server'));
